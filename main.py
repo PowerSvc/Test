2227d6fd57b77a942dafd8ed038789f21259d5a9
PowerSvc <sasha.vilichko.228@gmail.com>
2021-08-06 16:43:21 +0300

    test

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 url = 'https://www.youtube.com/'
 
-browser = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"))
 print('Открываю браузер')
 browser.get(url)
 time.sleep(2)
